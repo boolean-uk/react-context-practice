0564ca7b79b93314b32346afb99a7dbfbd8997cd
Leonardo Lodi <your_email@address>
2024-05-09 10:18:07 +0100

    do context component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import CreatePostForm from './components/CreatePostForm'
 import PostList from './components/PostList'
+import { PostsContext } from './components/PostsContext'
 
 function App() {
   const [posts, setPosts] = useState([
@@ -10,14 +11,14 @@ function App() {
   ])
 
   return (
-    <>
+    <PostsContext.Provider value={ { posts, setPosts } }>
       <header>
-        <CreatePostForm posts={posts} setPosts={setPosts} />
+        <CreatePostForm />
       </header>
       <main>
-        <PostList posts={posts} />
+        <PostList />
       </main>
-    </>
+    </PostsContext.Provider>
   )
 }
 
--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,4 +1,9 @@
-export default function PostList({ posts }) {
+import { useContext } from "react"
+import { PostsContext } from "./PostsContext"
+
+export default function PostList() {
+    const { posts } = useContext(PostsContext)
+
     return (
         <>
             <h2>Here be posts!</h2>
